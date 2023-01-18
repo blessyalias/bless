f28a4c8653cc783e8fcad34ae2ada19cf891ee2d
student <blessyalias2k@gmail.com>
2023-01-18 16:08:40 +0530

    sixth commit

---
+++ p6.sh
@@ -0,0 +1,18 @@
+echo "Enter 3 nos"
+read a 
+read b
+read c
+if [ $a -gt $b ]
+then
+if [ $a -gt $c ]
+then 
+echo "Largest is $a"
+else
+echo "Largest is $c"
+fi
+elif [ $b -gt $c ]
+then
+echo "Largest is $b"
+else
+echo "Largest is $c"
+fi
