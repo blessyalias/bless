3b7de9e916444f235c58846e5a19748baafd6938
student <blessyalias2k@gmail.com>
2023-02-08 15:48:52 +0530

    p10

---
+++ p10.sh
@@ -0,0 +1,14 @@
+echo " Enter a no"
+read a
+rev=0
+while [ $a -gt 0 ]
+do
+d=`expr $a % 10`
+if [ $d -eq 0 -a $rev -eq 0 ]
+then
+printf "$d"
+fi
+rev=`expr $rev \* 10 + $d`
+a=`expr $a / 10`
+done
+echo "Reverse is :$rev"
